9d64001a094adb79dfb31bb207ef551bf09f6e78
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-10 16:14:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const body = document.querySelector('body');
+const mainHeader = document.querySelector('#main');
+body.removeChild(mainHeader);
+
+let yourName = 'Harry';
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = `${yourName} is the champion`;
+body.appendChild(newHeader);
