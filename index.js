7144f60650e035dffb8cd339a3f5a5267e68464d
Mely07 <damely.tineo07@gmail.com>
2020-06-16 00:52:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1");
+
+// newHeader.setAttribute("id", "victory");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Damely is the champion"
