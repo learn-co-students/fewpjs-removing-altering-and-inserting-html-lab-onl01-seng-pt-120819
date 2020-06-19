f7dbb24c87a64660112c654b1bfb3debf8d2e615
asif-kamal <asif.kamal1489@gmail.com>
2020-06-19 13:07:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove()
+
+
+let newHeader = document.createElement('h1')
+let victory = document.createTextNode("<h1>YOUR-NAME is the champion</h1>")
+newHeader.setAttribute("id", "victory")
+
+newHeader.appendChild(victory)
+document.body.appendChild(newHeader)
