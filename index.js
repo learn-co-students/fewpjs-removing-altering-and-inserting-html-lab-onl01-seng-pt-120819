528d29a17aea3269d17fdadafe15bb4969053ad5
8ceg6 <8ceg6@comcast.net>
2020-06-21 12:20:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+document.querySelector('h1');
+newHeader.innerHTML = 'Cedric is the champion';
